fcc88abbe916cd1cb3076c6df25dfd3ae247be16
edallenjr <edallenaz@gmail.com>
2023-05-22 16:26:43 -0700

    Remove the sandbox account.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,51 +1,51 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
+# module "sandbox" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "ed+demo01-sandbox-aft@edallenjr.com"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "ed+demo01@edallenjr.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "ed+demo01-sandbox-aft@edallenjr.com"
+#     AccountName               = "sandbox-aft"
+#     ManagedOrganizationalUnit = "Learn AFT"
+#     SSOUserEmail              = "ed+demo01@edallenjr.com"
+#     SSOUserFirstName          = "Sandbox"
+#     SSOUserLastName           = "AFT"
+#   }
 
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
+#   account_tags = {
+#     "Learn Tutorial" = "AFT"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "HashiCorp Learn"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+#   }
 
-  custom_fields = {
-    group = "non-prod"
-    alternate_contact = jsonencode(
-      {
-        "billing"= {
-          "email-address" = "billing@mycompany.com",
-          "name"          = "Account Receiveable",
-          "phone-number"  = "+11234567890",
-          "title"         = "Billing Department"
-        },
-        "operations"= {
-          "email-address" = "ops@mycompany.com",
-          "name"          = "Operations 24/7",
-          "phone-number"  = "+11234567890",
-          "title"         = "DevOps Team"
-        },
-        "security"= {
-          "email-address" = "soc@mycompany.com",
-          "name"          = "Security Ops Center",
-          "phone-number"  = "+11234567890",
-          "title"         = "SOC Team"
-        }
-      }
-    )
-    another_custom_field1 = "a" #optional for illustration
-    another_custom_field1 = "b" #optional for illustration
-  }
+#   custom_fields = {
+#     group = "non-prod"
+#     alternate_contact = jsonencode(
+#       {
+#         "billing"= {
+#           "email-address" = "billing@mycompany.com",
+#           "name"          = "Account Receiveable",
+#           "phone-number"  = "+11234567890",
+#           "title"         = "Billing Department"
+#         },
+#         "operations"= {
+#           "email-address" = "ops@mycompany.com",
+#           "name"          = "Operations 24/7",
+#           "phone-number"  = "+11234567890",
+#           "title"         = "DevOps Team"
+#         },
+#         "security"= {
+#           "email-address" = "soc@mycompany.com",
+#           "name"          = "Security Ops Center",
+#           "phone-number"  = "+11234567890",
+#           "title"         = "SOC Team"
+#         }
+#       }
+#     )
+#     another_custom_field1 = "a" #optional for illustration
+#     another_custom_field1 = "b" #optional for illustration
+#   }
 
-  account_customizations_name = "sandbox"
-}
+#   account_customizations_name = "sandbox"
+# }
