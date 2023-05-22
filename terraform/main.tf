e4dab80e1a6c23c017f88ee0ea3aa766544b28bc
edallenjr <edallenaz@gmail.com>
2023-05-22 14:28:13 -0700

    Add alternate contacts.

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,30 @@ module "sandbox" {
 
   custom_fields = {
     group = "non-prod"
+    alternate_contact = jsonencode(
+      {
+        "billing"= {
+          "email-address" = "billing@mycompany.com",
+          "name"          = "Account Receiveable",
+          "phone-number"  = "+11234567890",
+          "title"         = "Billing Department"
+        },
+        "operations"= {
+          "email-address" = "ops@mycompany.com",
+          "name"          = "Operations 24/7",
+          "phone-number"  = "+11234567890",
+          "title"         = "DevOps Team"
+        },
+        "security"= {
+          "email-address" = "soc@mycompany.com",
+          "name"          = "Security Ops Center",
+          "phone-number"  = "+11234567890",
+          "title"         = "SOC Team"
+        }
+      }
+    )
+    another_custom_field1 = "a" #optional for illustration
+    another_custom_field1 = "b" #optional for illustration
   }
 
   account_customizations_name = "sandbox"
